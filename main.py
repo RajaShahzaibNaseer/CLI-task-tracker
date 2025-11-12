cc1580133f4601e1bf79ed0b83ce91e2fde9c624
RajaShahzaibNaseer <shahzaib.naseer.work2@gmail.com>
2025-11-12 18:51:34 +0500

    added persistent execution

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ import json
 
 def main():
     tasks = {}
-    choice = input("what do you want to do")
-    if choice == "add":
-        description = input("Please enter the description")
-        status = input("Please enter the status of your task")
-        created_at = "2025-11-12"
-        updated_at = "2025-11-12"
-        tasks = {
-            "id": 1,
-            "description": description,
-            "status": status,
-            "createdAt": created_at,
-            "updatedAt": updated_at
-        }
-        print(tasks)
+    working = True
+    while working:
+        choice = input("what do you want to do")
+        if choice == "add":
+            description = input("Please enter the description")
+            status = input("Please enter the status of your task")
+            created_at = "2025-11-12"
+            updated_at = "2025-11-12"
+            tasks = {
+                "id": 1,
+                "description": description,
+                "status": status,
+                "createdAt": created_at,
+                "updatedAt": updated_at
+            }
+            print(tasks)
+
+        if choice == "exit":
+            working = False
+            
 
 main()
